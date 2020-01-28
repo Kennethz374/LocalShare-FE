af525b8ceaca2b3cfa687d2b6eb2250ab9756490
Kenneth <sally@sallyde-MacBook-Air.local>
2020-01-28 00:12:39 -0800

    need to fix style

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -4,27 +4,25 @@ import UserItem from "../components/UserItem";
 
 //return Text No User Found when there is no user OR render users
 
-function UsersList (props) {
+function UsersList(props) {
   if (props.items.length === 0) {
-    return (
-      <h1>No User Found...</h1>
-    )
+    return <h1>No User Found...</h1>;
   }
   return (
     <ul className="users-list">
-    {props.items.map(user => {
-      return (
-        <UserItem
-          key={user.id}
-          id={user.id}
-          image={user.image}
-          name={user.name}
-          placeCount={user.places}
-        />
-      );
-    })}
-  </ul>
-  )
+      {props.items.map(user => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
+      })}
+    </ul>
+  );
 }
 
 export default UsersList;
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,9 +1,17 @@
 import React from "react";
-import UsersList from "../components/UsersList"
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USERS = [{id:"u1", name:"max", image:"123123", places:3}]
-  return <UsersList items={USERS}/>
+  const USERS = [
+    {
+      id: "u1",
+      name: "max",
+      image:
+        "https://specials-images.forbesimg.com/imageserve/5cd963805ea6490008c84023/960x0.jpg?fit=scale",
+      places: 3
+    }
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
