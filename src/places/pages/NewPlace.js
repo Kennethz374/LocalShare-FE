47b25cd684b53c43f6252426e1281601faccf492
Kenneth <sally@sallyde-MacBook-Air.local>
2020-02-06 00:15:25 -0800

    added simple form and simple validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
-  return <form></form>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter valid title"
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
+// add an input component which contains type, label, validators(validation logic), onChange
